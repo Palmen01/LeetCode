3d24deb9d10e3e43dead277c981eece5ff3e393d
Palmen01 <144669500+Palmen01@users.noreply.github.com>
2025-10-01 21:30:20 +0200

    2469 (Done)

---
+++ Sandbox/2469.CovertTheTemp.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.InteropServices.Marshalling;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var s = new Solution();
+        var caseOne = s.ConvertTemperature(36.5);
+        Console.WriteLine(string.Join(", ", caseOne));
+
+        var caseTwo = s.ConvertTemperature(122.11);
+        Console.WriteLine(string.Join(", ", caseTwo));
+
+    }
+}
+public class Solution {
+    public double[] ConvertTemperature(double celsius)
+    {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return [kelvin, fahrenheit];
+    }
+}
